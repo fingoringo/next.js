a4c027a0cdb4e6d956503b07f716d53219649c01
Pawel <pawelhilczer@gmail.com>
2025-03-05 11:56:48 +0100

    Css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import styles from './styles/home.module.css'
+
 export default function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Welcome</h1>
             <p>okok</p>
             <a href="/about">A propos</a>
