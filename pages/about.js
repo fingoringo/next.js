efcd5e5848e7b7c4f410aeec08eb81d4782732de
Pawel <pawelhilczer@gmail.com>
2025-03-05 11:49:12 +0100

    Premier site next

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <p>Next</p>
+            <a href="/">Retour accueuil</a>
+        </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>okok</p>
+            <a href="/about">A propos</a>
+        </div>
+        
+    );
+}
+
